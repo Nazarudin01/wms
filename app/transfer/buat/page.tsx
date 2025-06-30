0e66ae7f6e76e911c02cef1bb8fe9d39a6eec9df
Nazarudin01 <jayshn01@gmail.com>
2025-06-30 09:43:18 +0700

    update semua file terbaru

--- app/transfer/buat/page.tsx
+++ app/transfer/buat/page.tsx
@@ -76,7 +76,7 @@ export default function BuatTransferGudangPage() {
     setForm(f => ({ ...f, items: [...f.items, { barangId: "", satuan: "", stok: 0, jumlahTransfer: 1 }] }));
   };
   // Handler hapus barang
-  const handleRemoveBarang = idx => {
+  const handleRemoveBarang = (idx: number) => {
     setForm(f => ({ ...f, items: f.items.filter((_, i) => i !== idx) }));
   };
   // Handler barang change
